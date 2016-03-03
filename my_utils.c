1119cf1a683f9b74f150dafc994ef83fb3a71eb6
freedom <stefano.vettor@mac.com>
2016-03-04 00:06:47 +0100

    initial commit

---
+++ my_utils.c
@@ -0,0 +1,25 @@
+#include <sched.h>
+#include <string.h>
+#include "my_utils.h"
+
+int set_process_priority(int priority) {
+  	struct sched_param params;
+  	memset(&params, 0, sizeof(params));
+	
+	switch(priority) {
+		case DEFAULT_PRIORITY:
+			params.sched_priority = 0;
+			break;
+		case MAX_PRIORITY:
+			params.sched_priority = sched_get_priority_max(SCHED_FIFO);
+			break;
+		case MIN_PRIORITY:
+			params.sched_priority = sched_get_priority_min(SCHED_FIFO);
+			break;
+		default:
+			params.sched_priority = priority;
+			break;
+	}
+	
+  	return sched_setscheduler(0, SCHED_FIFO, &params);
+}
---
+++ my_utils.h
@@ -0,0 +1,10 @@
+#ifndef MY_UTILS_H
+#define MY_UTILS_H
+
+#define DEFAULT_PRIORITY 	0
+#define MIN_PRIORITY 		1
+#define MAX_PRIORITY		99
+
+int set_process_priority(int priority);
+
+#endif
